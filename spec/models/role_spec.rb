a367d7d283a29fa42d90bc00714acb63b54bd569
marcmentis <mmentis@optonline.net>
2015-11-09 13:01:22 -0500

    rspec roles wip

---
+++ spec/models/role_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "Role validation" do
+	it "has a valid factory" do
+		expect(build(:role)).to be_valid
+	end
+
+	it "should have many users" do
+		role = build(:role)
+		expect(role).to have_and_belong_to_many :users
+	end
+end
