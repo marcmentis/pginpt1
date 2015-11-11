652c02b5fb68232a866b5edd103fbc2080ee6c98
marcmentis <mmentis@optonline.net>
2015-11-11 13:43:44 -0500

    rspec wip

--- spec/controllers/patients_controller_spec.rb
+++ spec/controllers/patients_controller_spec.rb
@@ -1,27 +1,37 @@
-require "rails_helper"
+require 'rails_helper'
 
-
-describe PatientsController do
-	
-	# describe "POST #create" do
-	# 	# let(:patient) {build(:patient)}
-
-	# 	context "with valid attributes" do
-	# 		it  "saves the new patient in the database" do
-	# 			params = {"patient"=>{"updated_by"=>"Mentis M", "id"=>"", "firstname"=>"Aaaa", "lastname"=>"Aaalast", "identifier"=>"656743", "facility"=>"0013", "site"=>"81/101", "doa"=>"2015-11-10", "dob"=>"", "dod"=>""}}
-	# 	# byebug
-	# 			# post :create, format: :json
-	# 			# expect(post :create, format: :json, patient: attributes_for(:patient)).to change(Patient, :count).by(1)
-	# 			expect(post :create, format: :json, patient: params).to change(Patient, :count).by(1)
-	# 		end
-	# 	end
-	# end
-
+RSpec.describe PatientsController, :type => :controller do
 	describe 'json output' do
-		it "returns a json file" do
-			post :create, format: :json
-			expect(response.headers['Content-Type']).to match 'application/json'
+		it "responds successfully with HTTP 200 status code" do
+			get :index
+			expect(response).to be_success
+			expect(response).to have_http_status(200)
 		end
 	end
-
 end
+
+
+# describe PatientsController do
+	
+# 	# describe "POST #create" do
+# 	# 	# let(:patient) {build(:patient)}
+
+# 	# 	context "with valid attributes" do
+# 	# 		it  "saves the new patient in the database" do
+# 	# 			params = {"patient"=>{"updated_by"=>"Mentis M", "id"=>"", "firstname"=>"Aaaa", "lastname"=>"Aaalast", "identifier"=>"656743", "facility"=>"0013", "site"=>"81/101", "doa"=>"2015-11-10", "dob"=>"", "dod"=>""}}
+# 	# 	# byebug
+# 	# 			# post :create, format: :json
+# 	# 			# expect(post :create, format: :json, patient: attributes_for(:patient)).to change(Patient, :count).by(1)
+# 	# 			expect(post :create, format: :json, patient: params).to change(Patient, :count).by(1)
+# 	# 		end
+# 	# 	end
+# 	# end
+# 	describe 'json output' do
+# 		it "returns a json file" do
+# 			post :create, format: :json
+# 			expect(response.code).to eq(200)
+	
+# 		end
+# 	end
+
+# end
---
+++ spec/features/users_spec.rb
@@ -0,0 +1,9 @@
+# require 'rails_helper'
+
+# feature 'User management' do
+# 	scenario "adds a new user" do
+# 		visit users_path
+
+# 		# save_and_open_page
+# 	end
+# end
