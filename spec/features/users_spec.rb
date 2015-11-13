f76d809e1ec1f2189e4f314baa276f8a99e9e87c
marcmentis <mmentis@optonline.net>
2015-11-13 11:29:19 -0500

    rspec features wip

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,4 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
+
+	feature 'Home Page' do
+    	# given(:valid_session){{confirmed: 'authen_and_in_db'}}
+    	session = {confirmed: 'authen_and_in_db'}
+		scenario 'Visit Home Page' do
+			visit root_path
+			expect(page).to have_content "Inpatient"
+		end
+	end
+
+
 
 # feature 'User management' do
 # 	scenario "adds a new user" do
